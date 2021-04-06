e50be5f63d1d75f4f6f31436bc6462086353cece
Eduar Rugamas <juaneduardo021299@hotmail.com>
2021-04-06 12:15:12 -0600

    proyect init

---
+++ app/src/main/java/com/thedevexperto/farma_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.thedevexperto.farma_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
